34a2b23ee4ef83c5a6a7c58111cc9e3cea2bb01c
Ryan Caloras <rcaloras@alumni.cmu.edu>
2022-05-22 16:41:21 -0400

    Added twitter bootstrap to landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
   return <Component {...pageProps} />
 }
 
