1ec8cd584d6d7c5d6a53cb0c8678141c277d11fb
Ryan Caloras <rcaloras@alumni.cmu.edu>
2022-05-23 17:22:03 -0400

    Update image and remove unused dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Head from 'next/head';
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-
 export default function Home() {
   return (
     <main className="container-fluid top-container">
