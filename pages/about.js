e1edbafcf8be3169aa72d22cbf2d7dcb9bd0f25d
Ryan Caloras <rcaloras@alumni.cmu.edu>
2022-05-23 16:13:11 -0400

    Decent spot with two panels and basic copy section
    - Still need to add new copy, add links, and make more responsive
      appropriately

--- pages/about.js
+++ pages/about.js
@@ -9,7 +9,29 @@ export default function About() {
           <link rel="icon" href="/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1"/>
         </Head>
-        <div className="image hero-image"></div>
+        <div className="row flex-fill vh-100">
+            <div className="col-md-8 p-0">
+                <div className="hero-image"/>
+            </div>
+            <div className="col-md-4">
+                <div className="body-content">
+                    <section className="name-headline">
+                        <section>
+                            <h1 className="name">Ryan Caloras</h1>
+                        </section>
+                        <section>
+                            <p className='headline'>Engineering Manager and Entrepreneur in New York</p>
+                        </section>
+                        <div className="profile-divider"/>
+                    </section>
+                    <section className="bio">
+                        <p>Full time jump shooter, entrepreneur, and hacker. I like to start things, and build stuff.
+                                I'm a musician at heart and will talk about music for hours.
+                                Always have a bag packed for my next adventure.</p>
+                    </section>
+                </div>
+            </div>
+        </div>
       </main>
     );
   }
