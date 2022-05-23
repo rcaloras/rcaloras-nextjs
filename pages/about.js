423040aa99d2725d8a724d69e7328ce2ad432c40
Ryan Caloras <rcaloras@alumni.cmu.edu>
2022-05-23 11:46:05 -0400

    fix to className

--- pages/about.js
+++ pages/about.js
@@ -9,7 +9,7 @@ export default function About() {
           <link rel="icon" href="/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1"/>
         </Head>
-        <div class="image hero-image"></div>
+        <div className="image hero-image"></div>
       </main>
     );
   }
