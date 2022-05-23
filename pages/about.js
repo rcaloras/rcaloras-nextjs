2eb781316a49ede0a4bcd7cf2145d43d3cc5da70
Ryan Caloras <rcaloras@alumni.cmu.edu>
2022-05-22 20:56:33 -0400

    Adding images trying to get layout
    - Have basic image displaying on the side of page. Need to setup
      properly with bootstrap to scale like about.me

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function About() {
+    return (
+      <main className="container-fluid top-container">
+        <Head>
+          <title>Ryan Caloras</title>
+          <meta name="description" content="rcaloras.com" />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        </Head>
+        <div class="image hero-image"></div>
+      </main>
+    );
+  }
